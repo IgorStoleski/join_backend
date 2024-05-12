b6ca448b85069009b0819d254bc922b13ce74b0f
Igor Stoleski <72672441+IgorStoleski@users.noreply.github.com>
2024-05-12 23:14:33 +0200

    bugfix token LoginView

--- api/views.py
+++ api/views.py
@@ -183,7 +183,7 @@ class LoginView(ObtainAuthToken):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         user = serializer.validated_data['user']
-        token = Token.objects.get_or_create(user=user)
+        token, created = Token.objects.get_or_create(user=user)
         return Response({
             'token': token.key,
             'user_id': user.pk,
