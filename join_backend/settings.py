a9b1fc51d2d84db8ae247cc6969a0787d6b16ae4
Igor Stoleski <72672441+IgorStoleski@users.noreply.github.com>
2024-05-16 17:43:11 +0200

    add cors regex

--- join_backend/settings.py
+++ join_backend/settings.py
@@ -30,18 +30,11 @@ ALLOWED_HOSTS = [
     "127.0.0.1"
 ]
 
-CORS_ALLOWED_ORIGINS = [
-    'http://localhost:4200',
-    'http://127.0.0.1:4200',
-    'http://localhost:8000',
-    'http://127.0.0.1:8000',
-    'http://localhost:5500',
-    'http://127.0.0.1:5500',
-    'http://localhost:5501',
-    'http://127.0.0.1:5501'
+CORS_ALLOWED_ORIGINS = []
+
+CORS_ALLOWED_ORIGIN_REGEXES = [
+    r"^https://\w+\.domain\.com$",
 ]
-
-
 
 # Application definition
 
