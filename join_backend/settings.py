13b7039dbfb6fba5ed7ae943afd1f898c2873a5f
Igor Stoleski <72672441+IgorStoleski@users.noreply.github.com>
2024-09-17 23:41:52 +0200

    add allowed hosts

--- join_backend/settings.py
+++ join_backend/settings.py
@@ -15,6 +15,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env('SECRET_KEY')
 
+ALLOWED_HOSTS = [
+    'localhost',
+    'backend.kanban-join.de'
+]
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
