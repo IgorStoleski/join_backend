ebe3bab2a947cde3e2f5278baac87e1b002ccb47
Igor Stoleski <72672441+IgorStoleski@users.noreply.github.com>
2024-09-17 20:52:58 +0200

    update requirements

--- join_backend/settings.py
+++ join_backend/settings.py
@@ -1,5 +1,9 @@
 import sentry_sdk
 from pathlib import Path
+import environ
+
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -9,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-xy3q3(lc$6v_26m!nd65n_3g(&v*crsnk^l6o5s6q)=a=rt8ze'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
